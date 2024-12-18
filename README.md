da2895d0d3b0a5c246dd36c89c606740596d825a
sfiresht <saar.firshtein@nokia.com>
2024-12-18 11:32:19 +0200

    extra info on the MD

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # lego
 This is my lego project
+before executing on a hub you'll need to pair it with the laptop, 
 
-Starting
+**NOTE: for the move hub it's impossible to just pair with a bluetooth devie you'll have to do this from the pybricks website using their tool in this [link](https://code.pybricks.com/) explanation on how to use it is available [here](https://pybricks.com/learn/getting-started/pybricks-environment/)**
 
-pre-reqs
+
+Pre-reqs
+---------------------------
 Install pybricksdev
 Install the pybricks python module
 
 
 Coding 
+---------------------------
 
+
+
+Resources
+---------------------------
+https://github.com/pybricks
+
+https://github.com/FLL-Team-24277/FLL-Fall-2023-Masterpiece   > go to help
+
+
+
+Issues
+---------------------------
+
+https://github.com/pybricks/support/issues/505
--- move/simple.py
+++ move/simple.py
@@ -7,17 +7,23 @@ from pybricks.tools import wait
 
 # Initialize the Move Hub
 hub = MoveHub()
+
 #print("current battery voltage: {0}".format(battery.voltage()))
-# Initialize motors on ports A and B
+# Initialize motors on ports A and B (Builtin ports)
 motor_a = Motor(Port.A)
 motor_b = Motor(Port.B)
+
 motor_d = Motor(Port.D)
 sensor = ColorDistanceSensor(Port.C)
 
 # Run both motors for 2 seconds
-motor_a.run_time(500, 2000, then=Stop.HOLD,wait=False)
+# the wait=false flag tells the code not to wait to this line to finish, instead just ove on, that way the next motor will be running as wel at the same time
+motor_a.run_time(500, 2000, then=Stop.HOLD,wait=False)  
 motor_b.run_time(-500, 2000, then=Stop.HOLD)
+
+#move motor d for an angle
 #print(motor_d.angle())
+
 motor_d.run_angle(30,30, then=Stop.HOLD)
 wait(2000)
 motor_d.run_angle(60,-60, then=Stop.HOLD)
@@ -26,7 +32,6 @@ print("back on track")
 
 
 print(Color.RED)
-
 sensor.light.on(Color.RED)
 wait(5000)
 sensor.light.off()
@@ -34,6 +39,7 @@ sensor.light.off()
 # Say goodbye and give some time to send it.
 print("Goodbye!")
 wait(100)
+
 
 # Shut the hub down.
 hub.system.shutdown()
